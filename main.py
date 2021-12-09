43ed7567bf9fd061cc35a0edc1fbd5b430dea2ae
Mihai Dima <dimamihaimail@gmail.com>
2021-12-09 17:34:03 +0200

    ssds

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import argparse
 import cv2
 import imutils
 import time
-
-def nothing(x):
-    pass
+import utils
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
@@ -16,13 +14,7 @@ ap.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
 args = vars(ap.parse_args())
 
-cv2.namedWindow("Tracking")
-cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
-cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
-cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
-cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
-cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
-cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+utils.createTrackingWindow()
 
 greenLower = (10,100,100)
 greenUpper = (15,255,255)
@@ -45,13 +37,8 @@ while True:
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    l_h = cv2.getTrackbarPos("LH", "Tracking")
-    l_s = cv2.getTrackbarPos("LS", "Tracking")
-    l_v = cv2.getTrackbarPos("LV", "Tracking")
 
-    u_h = cv2.getTrackbarPos("UH", "Tracking")
-    u_s = cv2.getTrackbarPos("US", "Tracking")
-    u_v = cv2.getTrackbarPos("UV", "Tracking")
+    [l_h, l_s, l_v, u_h, u_s, u_v] = utils.getTrackbarPositions()
 
     l_b = np.array([l_h, l_s, l_v])
     u_b = np.array([u_h, u_s, u_v])
@@ -71,23 +58,14 @@ while True:
     cnts = imutils.grab_contours(cnts)
     center = None
 
-    if len(cnts) > 0:
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    utils.drawCircles(frame, cnts)
 
-        if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
     pts.appendleft(center)
 
     cv2.imshow("Frame", frame)
     cv2.imshow('Counturs', mask)
-    key = cv2.waitKey(1) & 0xFF
 
-    if key == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 if not args.get("video", False):
---
+++ utils.py
@@ -0,0 +1,37 @@
+import cv2
+
+def nothing(x):
+    pass
+
+def drawCircles(frame, cnts):
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+def createTrackingWindow():
+    cv2.namedWindow("Tracking")
+    cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+    cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+    cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+
+def getTrackbarPositions():
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    return (l_h, l_s, l_v, u_h, u_s, u_v)
