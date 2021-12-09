025bcd45ec104d3bb10834086de24abca6cf3051
Mihai Dima <dimamihaimail@gmail.com>
2021-12-09 13:21:06 +0200

    dasdasds

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ import cv2
 import imutils
 import time
 
+def nothing(x):
+    pass
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
 args = vars(ap.parse_args())
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
 greenLower = (10,100,100)
 greenUpper = (15,255,255)
@@ -26,42 +37,58 @@ else:
 time.sleep(2.0)
 
 while True:
-	frame = vs.read()
+    frame = vs.read()
+    frame = frame[1] if args.get("video", False) else frame
+    if frame is None:
+        break
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 
-	frame = frame[1] if args.get("video", False) else frame
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
 
-	if frame is None:
-		break
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
 
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
 
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
+    image = cv2.circle(frame, (0,0), 40, (l_h, l_s, l_v),32 )
+    cv2.imshow('Tracking', image)
 
-	if len(cnts) > 0:
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    image2 = cv2.circle(frame, (0,200), 40, (u_h, u_s, u_v),32 )
+    cv2.imshow('Tracking', image2)
 
-		if radius > 10:
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-	pts.appendleft(center)
+    mask = cv2.inRange(hsv, l_b, u_b)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
 
-	if key == ord("q"):
-		break
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    pts.appendleft(center)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow('Counturs', mask)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
 
 if not args.get("video", False):
 	vs.stop()
